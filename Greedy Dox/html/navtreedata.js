a695a4b4c4ddeffbbc5f9de08a18ed3ef51ae4c8
Hieracon <37873846+Hieracon@users.noreply.github.com>
2018-06-05 15:38:29 +0300

    Add files via upload

---
+++ Greedy Dox/html/navtreedata.js
@@ -0,0 +1,16 @@
+var NAVTREE =
+[
+  [ "Greedy Dox", "index.html", [
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"coin__change_8c_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
